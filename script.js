4c67702f0752b68b4ed27d007a3a513f5abb1d67
rahuladupa <rahul.adupa123@gmail.com>
2022-08-31 13:52:12 +0530

    dark mode

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.getElementById('#changeMode').addEventListener('click', function (e) {
+//     e.preventDefault();
+//     DarkMode();
+// })
+$('#changeMode').click(function(e) {
+    DarkMode();
+    return false;
+});
+const  DarkMode = (e) =>{
+          let mybody = document.body;
+          mybody.classList.toggle('mydark')
+        }
